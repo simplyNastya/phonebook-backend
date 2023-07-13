be6b42eec1c414792af83b44dd9d0720b50d1a9f
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-07-13 22:00:59 +0200

    Update server.js
    Changed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app')
 const mongoose = require('mongoose')
 
-const { DB_HOST, PORT = 3001 } = process.env
+const { DB_HOST, PORT=4001 } = process.env
 
 mongoose.connect(DB_HOST)
   .then(() => {
